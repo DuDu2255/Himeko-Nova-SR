cb2bad529556c378edfe7eb1897495feb8a9e4a3
DBKAHHK <abc88435832@gmail.com>
2026-01-25 20:58:31 -0800

    Delete fireflygo_proxy directory

--- fireflygo_proxy/run_admin.go
+++
@@ -1,7 +0,0 @@
-//go:build !windows && !darwin && !linux
-
-package main
-
-func runWithAdmin(exePath string, env []string) error {
-	return nil
-}
--- fireflygo_proxy/system_proxy.go
+++
@@ -1,7 +0,0 @@
-//go:build !windows && !darwin && !linux
-
-package main
-
-func setProxy(enable bool, host string, port string) error {
-	return nil
-}
